16eb69c62640862a5efbd3b3706a43c402328a8c
Climax Mba <climaxium150@gmail.com>
2023-09-19 15:29:18 +0100

    Setup buildTree method

--- index.js
+++ index.js
@@ -14,7 +14,19 @@ class Tree {
   }
 
   buildTree(arr) {
-    // TODO
-    return arr;
+    if (!arr.length) return null;
+    // Sort and remove duplicates
+    arr = arr.sort().filter((val, i, self) => self.indexOf(val) === i);
+
+    const mid = parseInt(arr.length/2);
+    const root = new Node(arr[mid]);
+
+    root.left = this.buildTree(arr.slice(0, mid));
+    root.right = this.buildTree(arr.slice(mid + 1));
+
+    return root;
   }
 }
+
+// Test
+console.log(new Tree([4, 5, 6, 2, 7, 7, 2]))
